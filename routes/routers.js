68ab7b1a59beb1f94fce42b18361decd637af765
PushkarKashyapi <codingpushkar@gmail.com>
2026-06-03 17:40:16 +0530

    rada

--- routes/routers.js
+++ routes/routers.js
@@ -1,18 +1,15 @@
 const express = require("express")
-app.use(cors())
-app.use(express.json())
+
 const {
   createWorkspace,
   getWorkspaces,
   deleteWorkspace
-} = require(
-  "../controllers"
-)
+} = require("../controller")
 
 const router = express.Router()
 
 router.post(
-  "/workspace",
+  "/",
   createWorkspace
 )
 
